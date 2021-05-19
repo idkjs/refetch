a2d5efda55cccd65a3dda56cdfdecf2c569569ab
Alain Armand <idkjs@users.noreply.github.com>
2021-05-19 15:37:33 +0200

    examples.re

---
+++ examples/examples.bs.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var Get = require("./get.bs.js");
+var Png = require("./png.bs.js");
+var Post = require("./post.bs.js");
+var Test = require("./test.bs.js");
+var Retry = require("./retry.bs.js");
+var Github = require("./github.bs.js");
+var Post_form = require("./post_form.bs.js");
+var Post_multipart = require("./post_multipart.bs.js");
+
+var Utils = Github.Utils;
+
+var FetchError = Github.FetchError;
+
+var Decode = Github.Decode;
+
+var columnify = Github.columnify;
+
+var getRepos = Github.getRepos;
+
+var printRepos = Github.printRepos;
+
+var retryGet = Retry.retryGet;
+
+exports.Utils = Utils;
+exports.FetchError = FetchError;
+exports.Decode = Decode;
+exports.columnify = columnify;
+exports.getRepos = getRepos;
+exports.printRepos = printRepos;
+exports.retryGet = retryGet;
+/* Get Not a pure module */
---
+++ examples/examples.re
@@ -0,0 +1,9 @@
+include Get;
+include Github;
+include Png;
+include Post_form;
+include Post_multipart;
+include Post;
+include Retry;
+
+include Test;
